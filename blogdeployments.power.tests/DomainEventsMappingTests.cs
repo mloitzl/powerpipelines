85300aa2204e7b8972936ed6a411005cefd5c09b
Martin Loitzl <martin@loitzl.com>
2021-12-12 15:16:01 +0100

    add unit test

---
+++ blogdeployments.power.tests/DomainEventsMappingTests.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using blogdeployments.domain.Events;
+using blogdeployments.power.Handler;
+using Xunit;
+
+namespace blogdeployments.power.tests;
+
+public class DomainEventsMappingTests
+{
+    [Fact]
+    public void Test_ShutdownInitiated_CheckHostStatusMapping_MapsCorrectly()
+    {
+        var configuration = new MapperConfiguration(cfg =>
+            cfg.CreateMap<ShutdownInitiated, CheckHostStatus>());
+        
+        configuration.AssertConfigurationIsValid();
+
+        var mapper = configuration.CreateMapper();
+        var @event = new ShutdownInitiated
+        {
+            Hostname = "asasdas"
+        };
+        
+        var expected = mapper.Map<CheckHostStatus>(@event);
+
+        Assert.Equal(expected.Hostname, @event.Hostname);
+    }
+}
