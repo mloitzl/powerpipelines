1fe61f8e363dcd09ea8944506a81ed8248dc3901
Martin Loitzl <martin@loitzl.com>
2022-10-04 13:06:11 +0200

    fix missing repository methods

--- blogdeployments.repository/DeploymentsRepository.cs
+++ blogdeployments.repository/DeploymentsRepository.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using blogdeployments.domain;
+using CouchDB.Driver.Extensions;
 
 namespace blogdeployments.repository;
 
@@ -26,5 +27,45 @@ public class DeploymentsRepository : IDeploymentsRepository
                 );
 
         return _mapper.Map<Deployment>(deploymentDoc);
+    }
+    public async Task<IEnumerable<Deployment>> GetDeployments()
+    {
+        // var lineCount = 0;
+        var tokenSource = new CancellationTokenSource();
+        // var x = await _context.Deployments.FindManyAsync(new []{ "55f3df55aa9067ae77ba8cb81b00076e" }, tokenSource.Token);
+        // var y = await _context.Deployments.Where(document => document.FriendlyName == "Hello World")
+        //     .ToListAsync(tokenSource.Token);
+        try
+        {
+            var x =
+                await
+                    _context
+                        .ClusterPower
+                        .Where(d => d.Id != null)
+                        .Take(1)
+                        .ToListAsync(tokenSource.Token);
+
+            return x.Select(document => _mapper.Map<Deployment>(document));
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+        // await foreach (var deploymentChange in _context
+        //                    .Deployments
+        //                    .GetContinuousChangesAsync(
+        //                        null, 
+        //                        null, 
+        //                        tokenSource.Token))
+        // {
+        //     lineCount++;
+        //     if (lineCount == 20) tokenSource.Cancel();
+        //     result.Add(_mapper.Map<Deployment>(deploymentChange.Document));
+        // }
+    }
+
+    public Task<IEnumerable<Deployment>> GetDeployment(string id)
+    {
+        throw new NotImplementedException();
     }
 }
--- blogdeployments.repository/IDeploymentsRepository.cs
+++ blogdeployments.repository/IDeploymentsRepository.cs
@@ -5,4 +5,6 @@ namespace blogdeployments.repository;
 public interface IDeploymentsRepository
 {
     Task<Deployment> CreateDeployment(Deployment deployment);
+    Task<IEnumerable<Deployment>> GetDeployments();
+    Task<IEnumerable<Deployment>> GetDeployment(string id);
 }
